3176ba16ea6c58e9b4595cf6028aca30fc7a5ce2
unknown <kathawalearyan9@gmail.com>
2022-02-03 18:37:57 +0530

    ggnori

---
+++ gurukul-sarva-vidya-niketan/src/App.js
@@ -0,0 +1,27 @@
+// import logo from './logo.svg';
+import bitty from "./chotibitty.jpg";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      hii  this works 
+      {/* <header className="App-header">
+        <img src={bitty} className="App-logo" alt="logo" />
+        <p>
+          hii im Acchita "bitty" singh
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn About Me 
+        </a>
+      </header> */}
+    </div>
+  );
+}
+
+export default App;
